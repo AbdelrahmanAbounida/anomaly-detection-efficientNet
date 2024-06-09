699e84211238d1a74df03734baac73d9b05abb78
aboneda <abdelrahmanaboneda@gmail.com>
2024-06-09 10:17:37 +0200

    change model layers

--- models/anomaly_detection_model.py
+++ models/anomaly_detection_model.py
@@ -5,18 +5,26 @@ class AnomalyModel(nn.Module):
     def __init__(self, num_classes=5):
         super(AnomalyModel, self).__init__()
         self.model = timm.create_model("efficientnet_b0", pretrained=True)
-        efficient_net_out_size = self.model.classifier.in_features  # Dynamically get the output features size
+        efficient_net_out_size = 1280  # efficientnet output features size
 
-        # Custom classifier
+        self.features = nn.Sequential(
+            *list(self.model.children())[:-1]
+        )  # remove last classification layer
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))  # Global average pooling layer
+
+        self.dropout = nn.Dropout(0.5)  # Dropout layer with 0.5 dropout rate
+
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(efficient_net_out_size, num_classes)
+            nn.Linear(efficient_net_out_size, num_classes),
+            nn.Softmax(dim=1)  # Softmax activation layer
         )
 
-        # Remove the last classification layer of the pretrained model
-        self.features = nn.Sequential(*list(self.model.children())[:-1])
-
     def forward(self, x):
-        x = self.features(x)  # Use EfficientNet layers and calculate them first
-        output = self.classifier(x)  # This is the last layer which takes 1280 and returns the number of classes
+        x = self.features(x)
+        # x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        output = self.classifier(x)
         return output
